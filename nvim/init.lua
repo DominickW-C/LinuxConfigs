6c7be7a819a4cfed771ff3f389668e7da8285391
DominickW-C <dwcouture04@gmail.com>
2025-05-28 21:25:11 +0000

    symlinks don't make sense

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,8 @@
-nvim/init.lua
+require("config.lazy")
+
+vim.cmd.colorscheme "catppuccin-frappe"
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set expandtab")
+vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set number")
