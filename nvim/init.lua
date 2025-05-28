b7222a6d3360b32193dc0e7d4aadef36d7846a97
DominickW-C <dwcouture04@gmail.com>
2025-05-28 21:21:00 +0000

    added more configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,1 @@
-require("config.lazy")
-
-vim.cmd.colorscheme "catppuccin-frappe"
-vim.cmd("set shiftwidth=4")
-vim.cmd("set tabstop=4")
-vim.cmd("set expandtab")
-vim.cmd("set clipboard+=unnamedplus")
-vim.cmd("set number")
-
+nvim/init.lua
