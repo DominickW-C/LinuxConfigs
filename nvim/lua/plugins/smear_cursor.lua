db887339ffbfb2961ffeacaccaf97eff65f8c1f6
DominickW-C <dwcouture04@gmail.com>
2025-06-20 16:12:47 +0000

    changed smear cursor a little

--- nvim/lua/plugins/smear_cursor.lua
+++ nvim/lua/plugins/smear_cursor.lua
@@ -1,10 +1,10 @@
 return {
     "sphamba/smear-cursor.nvim",
     opts = {
-        stiffness = 0.4,
+        stiffness = 0.5,
         trailing_stiffness = 0.2,             
         stiffness_insert_mode = 0.3,          
         trailing_stiffness_insert_mode = 0.3, 
         distance_stop_animating = 0.1,      },
-        time_interval = 7
+        time_interval = 10 
 }
